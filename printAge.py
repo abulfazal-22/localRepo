589185f9cb247c479656f72a0dc8ccecaa981366
abulfazal-22 <145914241+abulfazal-22@users.noreply.github.com>
2025-08-03 17:21:01 +0530

    Changes to printAge1

--- printAge.py
+++ printAge.py
@@ -2,4 +2,4 @@ m=int(input("Enter your Age: "))
 print("You are " , m, "years old.")
 
 n=int(input("Enter your JEE%ile: "))
-print("You got " , n, "%ile in JEE.")
+print("You got " , n,m,  "%ile in JEE.")
